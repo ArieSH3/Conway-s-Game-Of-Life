168fb7f3ebd74b04fe8e3791cea517e7a46d7f5f
ArieSH3 <StepDubUp@gmail.com>
2021-05-26 17:58:37 +0200

    First iteration of the grid creation

--- Convways_Game_Of_Life.py
+++ Convways_Game_Of_Life.py
@@ -1,5 +1,8 @@
 '''
-	Creating a Conway's game of life to be run in terminal with ASCII characters
+	Author: Stefan Popovic
+	Date  : 26/05/2021
+
+	About : Creating a Conway's game of life to be run in terminal with ASCII characters
 
 	Rules:
 		1. Any live cell with two or three live neighbours survives
@@ -8,8 +11,10 @@
 
 		** All cells with no pixel(character) are considered dead and can be revived regardless if
 		   there was or wasn't a character alive there before.
-
-	Will maybe use nested lists for grid or just use one list and manually set that each
+	
+	How it will work:
+	
+		Will maybe use nested lists for grid or just use one list and manually set that each
 	row has for example 10 pixels(characters) so it might be easier to add and subtract 10 +-1
 	to determine if there are any neighbours above, below or diagonally above or below with.
 
@@ -31,3 +36,32 @@
 								  logs how many of them there are and according to the rules it 
 								  either lives or dies.
 '''
+
+import numpy # Will use to speed up looping through nested lists
+import pprint
+
+
+# Will be used for both a number of elements in nested list and the number of nested lists
+grid_size = 20 
+
+outer_list = []
+no_element = ' '
+element = '#'
+
+# Setting up pretty printer so nested lists are printed below eachother instead of next to eachother
+pp = pprint.PrettyPrinter(indent=1)
+
+for i in range(grid_size):
+	nested_list = []
+	for j in range(grid_size):
+		if i==grid_size-1 and j>grid_size-2 and j<grid_size+2:
+			nested_list.append(element)
+		else:
+			nested_list.append(no_element)
+
+	outer_list.append(nested_list)
+
+pp.pprint(outer_list)
+input()
+# print(outer_list)
+
